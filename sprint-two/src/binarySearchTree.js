b53163a0f1950a4e21ea1864a447f77a81a28888
Hack Reactor Student <student@hackreactor.com>
2019-04-12 21:04:31 -0700

    began solution for binary tree search

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,30 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function (value) {
+  this[value] = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function (value) {
+  var bsTree = new BinarySearchTree(value);
+  var root = this.value;
+  if (value < root) {
+    if (this.left === null) {
+      this.left = bsTree;
+    }
+    //recursion!!
+  } else {
+    this.right = bsTree;
+  }
 
+};
+
+BinarySearchTree.prototype.contains = function (target) {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function () {
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
