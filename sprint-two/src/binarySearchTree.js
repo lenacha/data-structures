890c920f7dd4d84fe426bd892b4e5adbc52f7a03
Yerin Cha <lenacha32@gmail.com>
2020-05-09 14:50:33 -0700

    re-do sprint-one

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,131 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
 
+BinarySearchTree.prototype.insert = function (value) {
+  var bsTree = new BinarySearchTree(value)
+
+  var insertion = function (bsNode) {
+    if(bsNode.value > value) {
+      if(bsNode.left === null) {
+        bsNode.left = bsTree;
+        return;
+      } else {
+        insertion(bsNode.left);
+      }
+    } else if (bsNode.value < value) {
+      if(bsNode.right === null) {
+        bsNode.right = bsTree;
+        return
+      } else {
+        insertion(bsNode.right);
+      }     
+    }
+  }
+
+  insertion(this);
+};
+
+BinarySearchTree.prototype.contains = function (target) {
+
+  var searching = function (bsNode) {
+    if(bsNode.value === target) {
+      return true
+    } 
+    if (bsNode.left) {
+      if(searching(bsNode.left)) {
+        return true;
+      }
+    } 
+    if (bsNode.right) {
+      if(searching(bsNode.right)) {
+        return true
+      }
+    }
+    return false;
+  }
+  return searching(this)
+}
+
+BinarySearchTree.prototype.depthFirstLog = function (cb) {
+  
+  var traverse = function (tree) {
+    cb(tree.value);
+    if(tree.left) {
+      traverse(tree.left);
+    }
+    if(tree.right)
+      traverse(tree.right);
+  }
+  
+  return traverse(this)
+  
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var binary = new BinarySearchTree(5)
+binary.insert(7)
+binary.insert(4)
+binary.insert(3)
+binary.insert(8)
+binary.insert(6)
+binary.insert(2)
+binary.insert(5)
+binary.insert(9)
+
+const findLargestLevel = function(node) {
+  var sumForLevel = {}
+   
+  function findSum (firstNode, level) {
+    
+    if(level === 0) {
+      sumForLevel[level] = firstNode.value
+    } else if(sumForLevel[level]===undefined){
+      if(firstNode.left !== null && firstNode.right !== null){
+        sumForLevel[level] = firstNode.left.value + firstNode.right.value
+      } else if(firstNode.left === null && firstNode.right !== null) {
+        sumForLevel[level] = firstNode.right.value
+      } else if(firstNode.right === null && firstNode.left !== null) {
+        sumForLevel[level] = firstNode.left.value
+      }
+      
+    } else {
+      if(firstNode.left !== null && firstNode.right !== null){
+        sumForLevel[level] += firstNode.left.value + firstNode.right.value
+      } else if(firstNode.left === null && firstNode.right !== null) {
+        sumForLevel[level] += firstNode.right.value
+      } else if(firstNode.right === null && firstNode.left !== null) {
+        sumForLevel[level] += firstNode.left.value
+      }
+    }
+
+    if(firstNode.left !== null){
+      findSum(firstNode.left, level+1);
+    }
+    if(firstNode.right !== null){
+      findSum(firstNode.right, level+1);
+    }
+     return;
+  }
+
+  findSum(node, 0)
+
+  var largestLevel = 0
+  var largestSum = Number.MIN_SAFE_INTEGER
+
+  for(var key in sumForLevel) {
+    if(sumForLevel[key] > largestSum){
+      largestSum = sumForLevel[key]
+      largestLevel = key
+    }
+  }
+
+  return largestLevel
+};
+
+console.log(findLargestLevel(binary))
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,12 +12,28 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newTree = Tree(value)
+  this.children.push(newTree)
 };
 
 treeMethods.contains = function(target) {
+  
+  var searching = function(tree) {
+  	if(tree.value === target) {
+  		return true;
+  	} else if (tree.children.length > 0) {
+	  for (var i = 0 ; i < tree.children.length; i++) {
+	    if(searching(tree.children[i])) {
+	    	return true;
+	    }
+	  }
+  	} 
+  	return false;
+  }
+
+  return searching(this);
+
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
