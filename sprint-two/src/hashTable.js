f83304b789c5b3a63efd8b6c2254960789dcaf33
Yerin Cha <lenacha32@gmail.com>
2020-05-13 21:34:06 -0700

    Make the hashTable double in size filled over 75 percent of the spaces and halve in size when space usage falls below 25 percent

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,29 +1,70 @@
 var HashTable = function () {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._load = 0;
+  this._loadIndex = 0;
 };
 
 HashTable.prototype.insert = function (k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  console.log(this._storage.get(index));
   if (this._storage.get(index) !== undefined) {
     let obj = this._storage.get(index);
-    obj[k] = v;
+    obj[k] = [k, v];
   } else {
     let newObj = {};
-    newObj[k] = v;
+    newObj[k] = [k, v];
     this._storage.set(index, newObj);
+  }
+  this._load += 1;
+  this._loadIndex = this._load / this._limit;
+  if (this._loadIndex > 0.75) {
+    let retrived = this.retrieveAll();
+    this._limit *= 2;
+    this._storage = LimitedArray(this._limit);
+    this._load = 0;
+    this.insertAll(retrived);
   }
 };
 
 HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index)[k];
+  if (this._storage.get(index) !== undefined) {
+    return this._storage.get(index)[k][1];
+  }
 };
 
 HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   let obj = this._storage.get(index);
   delete obj[k];
+  this._load -= 1;
+  this._loadIndex = this._load / this._limit;
+
+  if (this._loadIndex < 0.25) {
+    let retrived = this.retrieveAll();
+    this._limit /= 2;
+    this._storage = LimitedArray(this._limit);
+    this._load = 0;
+    this.insertAll(retrived);
+  }
+};
+
+HashTable.prototype.retrieveAll = function () {
+  let retrived = [];
+  this._storage.each((tuple) => {
+    for (let key in tuple) {
+      retrived.push(tuple[key]);
+    }
+  });
+
+  return retrived;
+};
+
+HashTable.prototype.insertAll = function (array) {
+  for (let i = 0; i < array.length; i++) {
+    this.insert(array[i][0], array[i][1]);
+  }
 };
 
 /*
